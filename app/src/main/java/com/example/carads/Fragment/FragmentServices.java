455e51487b86824ae2b4ee1b6d0800b49b820349
Your Name <you@example.com>
2021-01-24 11:50:46 +0200

    18-1-2021

--- app/src/main/java/com/example/carads/Fragment/FragmentServices.java
+++ app/src/main/java/com/example/carads/Fragment/FragmentServices.java
@@ -1,66 +1,72 @@
 package com.example.carads.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.carads.Adapter.AdapterNotification;
+import com.example.carads.Adapter.AdapterService;
+import com.example.carads.Models.ModelNotification;
+import com.example.carads.Models.ModelService;
 import com.example.carads.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentServices#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class FragmentServices extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FragmentServices() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentServices.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentServices newInstance(String param1, String param2) {
-        FragmentServices fragment = new FragmentServices();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    View view;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_services, container, false);
+        view = inflater.inflate(R.layout.fragment_services, container, false);
+        init();
+        return view;
     }
+
+
+    RecyclerView recyclerView;
+    AdapterService adapterService;
+    ArrayList<ModelService> arrayList;
+
+    GridLayoutManager gridLayoutManager;
+    private void init() {
+        arrayList = new ArrayList<>();
+        recyclerView = view.findViewById(R.id.recycler);
+        gridLayoutManager = new GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        for (int i = 0; i < 10; i++) {
+            ModelService item = new ModelService();
+            item.setId(i+"");
+
+            arrayList.add(item);
+        }
+        adapterService = new AdapterService(getActivity(),arrayList);
+        recyclerView.setAdapter(adapterService);
+    }
+    Context context;
+    @Override
+    public void onAttach(@NonNull Context context) {
+        this.context = context;
+        super.onAttach(context);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/carads/Models/ModelService.java
@@ -0,0 +1,14 @@
+package com.example.carads.Models;
+
+public class ModelService {
+    private String id;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
